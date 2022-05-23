71f92e7c747c5aa2393102302a1d352f3018faf6
YamadaHideki <top.secret.work@yandex.ru>
2022-05-23 16:16:50 +0300

    Refactor code

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -11,32 +11,32 @@ import java.util.concurrent.atomic.AtomicLong;
 @Repository
 public class PostRepository {
 
-  private final Map<Long, Post> data = new ConcurrentHashMap<>();
-  private final AtomicLong counter = new AtomicLong();
+    private final Map<Long, Post> data = new ConcurrentHashMap<>();
+    private final AtomicLong counter = new AtomicLong();
 
-  public List<Post> all() {
-    return new ArrayList<>(data.values());
-  }
-
-  public Optional<Post> getById(long id) {
-    return Optional.ofNullable(data.get(id));
-  }
-
-  public Post save(Post post) {
-    if (post.getId() == 0) {
-      post.setId(counter.addAndGet(1));
-      data.putIfAbsent(post.getId(), post);
-      return post;
-    } else if(post.getId() > 0) {
-      getById(post.getId()).ifPresent(
-              e -> e.setContent(post.getContent())
-      );
-      return data.get(post.getId()) != null && data.get(post.getId()).getContent().equals(post.getContent()) ? post : null;
+    public List<Post> all() {
+        return new ArrayList<>(data.values());
     }
-    return null;
-  }
 
-  public void removeById(long id) {
-    data.remove(id);
-  }
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(data.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(counter.addAndGet(1));
+            data.putIfAbsent(post.getId(), post);
+            return post;
+        } else if (post.getId() > 0) {
+            getById(post.getId()).ifPresent(
+                    e -> e.setContent(post.getContent())
+            );
+            return data.get(post.getId()) != null && data.get(post.getId()).getContent().equals(post.getContent()) ? post : null;
+        }
+        return null;
+    }
+
+    public void removeById(long id) {
+        data.remove(id);
+    }
 }
--- src/main/java/ru/netology/service/PostService.java
+++ src/main/java/ru/netology/service/PostService.java
@@ -9,26 +9,26 @@ import java.util.List;
 
 @Service
 public class PostService {
-  private final PostRepository repository;
+    private final PostRepository repository;
 
-  public PostService(PostRepository repository) {
-    this.repository = repository;
-  }
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
 
-  public List<Post> all() {
-    return repository.all();
-  }
+    public List<Post> all() {
+        return repository.all();
+    }
 
-  public Post getById(long id) {
-    return repository.getById(id).orElse(null);
-  }
+    public Post getById(long id) {
+        return repository.getById(id).orElse(null);
+    }
 
-  public Post save(Post post) {
-    return repository.save(post);
-  }
+    public Post save(Post post) {
+        return repository.save(post);
+    }
 
-  public void removeById(long id) {
-    repository.removeById(id);
-  }
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
 }
 
